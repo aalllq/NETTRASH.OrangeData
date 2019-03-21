017af4f3c4d2b09b74cbc1a0ec9c18889f835f6b
NETTRASH <nettrash@nettrash.ru>
2019-03-21 15:24:50 +0300

    Initial commit

---
+++ NETTRASH.OrangeData.Nebula/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NETTRASH.OrangeData.Nebula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
