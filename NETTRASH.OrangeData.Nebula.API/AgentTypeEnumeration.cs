3c2021eabbfcf8af2b0e0e11fe492bec97d99e3e
nettrash <nettrash@nettrash.ru>
2019-05-08 13:35:51 +0300

    refactor

---
+++ NETTRASH.OrangeData.Nebula.API/AgentTypeEnumeration.cs
@@ -0,0 +1,40 @@
+﻿namespace NETTRASH.OrangeData.Nebula.API
+{
+    public enum AgentTypeEnumeration : int
+    {
+        /// <summary>
+        /// Банковский платёжный агент
+        /// </summary>
+        BankPaymentAgent = 1,
+
+        /// <summary>
+        /// Банковский платёжный субагент
+        /// </summary>
+        BankPaymentSubagent = 2,
+
+        /// <summary>
+        /// Платёжный агент
+        /// </summary>
+        PaymentAgent = 4,
+
+        /// <summary>
+        /// Платёжный субагент
+        /// </summary>
+        PaymentSubagent = 8,
+
+        /// <summary>
+        /// Поверенный
+        /// </summary>
+        Attorney = 16,
+
+        /// <summary>
+        /// Комиссионер
+        /// </summary>
+        Commissioner = 32,
+
+        /// <summary>
+        /// Иной агент
+        /// </summary>
+        Other = 64
+    }
+}
---
+++ NETTRASH.OrangeData.Nebula.API/KKTOperationModeEnumeration.cs
@@ -0,0 +1,35 @@
+﻿namespace NETTRASH.OrangeData.Nebula.API
+{
+    public enum KKTOperationModeEnumeration : int
+    {
+        /// <summary>
+        /// Шифрование
+        /// </summary>
+        Encryption = 1,
+
+        /// <summary>
+        /// Автономный режим
+        /// </summary>
+        Offline = 2,
+
+        /// <summary>
+        /// Автоматический режим
+        /// </summary>
+        Automatic = 4,
+
+        /// <summary>
+        /// Применение в сфере услуг
+        /// </summary>
+        ApplicationInTheServiceSector = 8,
+
+        /// <summary>
+        /// Режим БСО
+        /// </summary>
+        BSO = 16,
+
+        /// <summary>
+        /// Применение в сети Интернет
+        /// </summary>
+        ApplicationInTheInternet = 32
+    }
+}
