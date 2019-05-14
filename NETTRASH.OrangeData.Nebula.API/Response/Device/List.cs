8f703f6a239eb4cc77218b4c3bd4ac10e3671af7
nettrash <nettrash@nettrash.ru>
2019-05-14 10:10:58 +0300

    Base device responses

---
+++ NETTRASH.OrangeData.Nebula.API/Response/Device/List.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace NETTRASH.OrangeData.Nebula.API.Response.Device
+{
+    [JsonObject]
+    public class List : BaseList<Classes.Device.Info>
+    {
+        #region Public constructors
+
+
+
+        public List()
+            : base()
+        {
+        }
+
+
+
+        #endregion
+        #region Public methods
+
+
+
+        public static List Load(string sSourceJson)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<List>(sSourceJson);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+
+        #endregion
+    }
+}
